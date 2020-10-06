60a53a0b1d67ce3649c8795b7b7bcf9dfd1da003
BariqDharmawan <bariq.2nd.rodriguez@gmail.com>
2020-10-06 17:14:42 +0700

    [UPDATE] performance for better view

--- resources/views/web/headers/cartButtons/cartButton9.blade.php
+++ resources/views/web/headers/cartButtons/cartButton9.blade.php
@@ -1,77 +1,73 @@
-<?php $qunatity=0; ?>
+@php $qunatity=0; @endphp
 @foreach($result['commonContent']['cart'] as $cart_data)
     <?php $qunatity += $cart_data->customers_basket_quantity; ?>
 @endforeach
 
-<button id="dropdownCartButton" class="btn dropdown-toggle" type="button" id="headerOneCartButton"  data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" > 
+<button id="dropdownCartButton" class="btn dropdown-toggle" type="button" id="headerOneCartButton"
+data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
     <div class="cart-left">
-     <i class="fas fa-shopping-bag"></i>
-     <span class="badge badge-secondary">{{ $qunatity }}</span>
+        <i class="fas fa-shopping-bag"></i>
+        <span class="badge badge-secondary">{{ $qunatity }}</span>
     </div>
- 
-       <div class="cart-right d-flex flex-column align-self-end ml-13">
-         <span class="title-cart">@lang('website.My Cart')</span>
-         <span class="cart-item"> @lang('website.items')</span>
-       </div>
-   </button> 
-   @if(count($result['commonContent']['cart'])>0)
+    <div class="cart-right d-flex flex-column align-self-end ml-13">
+        <span class="title-cart">@lang('website.My Cart')</span>
+        <span class="cart-item"> @lang('website.items')</span>
+    </div>
+</button>
 
+@if(count($result['commonContent']['cart'])>0)
 <div class="shopping-cart shopping-cart-empty dropdown-menu dropdown-menu-right" aria-labelledby="dropdownCartButton_1">
     <ul class="shopping-cart-items">
-        <?php
+        @php
             $total_amount=0;
             $qunatity=0;
-        ?>
+        @endphp
         @foreach($result['commonContent']['cart'] as $cart_data)
-
-        <?php
-                     $total_amount += $cart_data->final_price*$cart_data->customers_basket_quantity;
-                    $qunatity 	  += $cart_data->customers_basket_quantity; ?>
-        <li>
-            <div class="item-thumb">
-               <div class="image">
-                    <img class="img-fluid" src="{{asset('').$cart_data->image}}" alt="{{$cart_data->products_name}}"/>
+            @php
+                $total_amount += $cart_data->final_price*$cart_data->customers_basket_quantity;
+                $qunatity += $cart_data->customers_basket_quantity;
+            @endphp
+            <li>
+                <div class="item-thumb">
+                    <div class="image">
+                        <img class="img-fluid" src="{{asset('') . $cart_data->image}}" alt="{{$cart_data->products_name}}">
+                    </div>
                 </div>
-            </div>
-            <div class="item-detail">
-                <h3 class="item-name">{{$cart_data->products_name}}</h3>
-                <div class="item-s">
-                    {{ $cart_data->customers_basket_quantity }} x 
-                    {{ 'IDR ' . number_format($cart_data->final_price*session('currency_value'), 0, ',', '.') }}
-                <a href="{{ URL::to('/deleteCart?id='.$cart_data->customers_basket_id)}}">
-                    <i class="fas fa-trash"></i>
-                </a>
-            </div>
-           </div>
-        </li>
+                <div class="item-detail">
+                    <h3 class="item-name">{{$cart_data->products_name}}</h3>
+                    <div class="item-s">
+                        {{ $cart_data->customers_basket_quantity }} x
+                        {{ 'IDR ' . number_format($cart_data->final_price*session('currency_value'), 0, ',', '.') }}
+                        <a href="{{ URL::to('/deleteCart?id='.$cart_data->customers_basket_id)}}">
+                            <i class="fas fa-trash"></i>
+                        </a>
+                    </div>
+                </div>
+            </li>
         @endforeach
         <li style="text-align: center">
             <span class="item-summary">
                 @lang('website.SubTotal')
-                <span>    
+                <span>
                     {{ 'IDR ' . number_format($total_amount*session('currency_value'), 0, ',', '.') }}
                 </span>
             </span>
         </li>
-    
+
         <li>
-              <a class="btn btn-link btn-block" href="{{ URL::to('/viewcart')}}">@lang('website.View Cart')</a>
-              <a class="btn btn-secondary btn-block swipe-to-top" href="{{ URL::to('/checkout')}}">@lang('website.Checkout')</a>     
-       </li>
- </ul>
-
+            <a class="btn btn-link btn-block" href="{{ URL::to('/viewcart')}}">
+                @lang('website.View Cart')
+            </a>
+            <a class="btn btn-secondary btn-block swipe-to-top" href="{{ URL::to('/checkout')}}">
+                @lang('website.Checkout')
+            </a>
+        </li>
+    </ul>
 </div>
-
 @else
-
 <div class="shopping-cart shopping-cart-empty dropdown-menu dropdown-menu-right" aria-labelledby="dropdownMenuButton">
     <ul class="shopping-cart-items">
         <li>@lang('website.You have no items in your shopping cart')</li>
     </ul>
 </div>
 @endif
-
-   <!--
-<script src="https://code.jquery.com/jquery-1.9.1.js"></script>
-<script src="https://code.jquery.com/ui/1.11.0/jquery-ui.js"></script>
--->
